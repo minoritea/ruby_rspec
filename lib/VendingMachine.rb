2dc7ce37367ff33bf48d78def33d796a17b60d8b
muratayusuke <info@muratayusuke.com>
2013-07-27 16:17:37 +0900

    implement step 1 and 2

--- lib/VendingMachine.rb
+++ lib/VendingMachine.rb
@@ -1,14 +1,29 @@
 class VendingMachine
   attr_reader :total
+  attr_reader :change
+  attr_reader :stock
+
   def initialize
     @total = 0
+    @change = 0
+    @stock = {coke: {price: 120, amount: 5}}
   end
+
   def add n
-    @total += n
+    if is_valid_money n
+      @total += n
+    else
+      @change += n
+    end
   end
+
+  def is_valid_money n
+    [10, 50, 100, 500, 1000].include?(n)
+  end
+
   def payback
-    change = @total
+    @change += @total
     @total = 0
-    change
+    @change
   end
 end
--- spec/lib/VendingMachine_spec.rb
+++ spec/lib/VendingMachine_spec.rb
@@ -8,7 +8,11 @@ describe VendingMachine do
     it '何も投入しなければ合計0件' do
       vm.total.should eq 0
     end
+    it '何も投入しなければつり銭ボックスは' do
+      vm.change.should eq 0
+    end
   end
+  
   context 'コインを投入したときの合計値についてのテスト' do
     it '10円入れたら合計10円' do
       vm.add 10
@@ -31,6 +35,7 @@ describe VendingMachine do
       vm.total.should eq 1000
     end
   end
+  
   context '複数回入れた時は合計金額がその合計になる' do
     it '10円と50円ならば合計60円' do
       vm.add 10
@@ -44,11 +49,39 @@ describe VendingMachine do
       vm.total.should eq 1600
     end
   end
+  
   context '釣り銭は合計金額を返して、内部合計金額を0にする' do
     it '100円投入して、100円帰ってきて、内部の合計は0になる' do
       vm.add 100
       vm.payback.should eq 100
       vm.total.should eq 0
+      vm.change.should eq 100
+    end
+  end
+  
+  context '想定していないお金が投入された時' do
+    it '1円を入れると、1円返ってきて、合計が0円のまま' do
+      vm.add 1
+      vm.total.should eq 0
+      vm.change.should eq 1
+    end
+    it '5円を入れると、5円返ってきて、合計が0円のまま' do
+      vm.add 5
+      vm.total.should eq 0
+      vm.change.should eq 5
+    end
+    it '5000円を入れると、5000円返ってきて、合計が0円のまま' do
+      vm.add 5000
+      vm.total.should eq 0
+      vm.change.should eq 5000
+    end
+  end
+  
+  describe 'ジュース情報の取得' do
+    context '初期状態の時' do
+      it '120円のコーラが5本ある' do
+        vm.stock.should eq coke: {price: 120, amount: 5}
+      end
     end
   end
 end
